4c989754bc9d3faf581a871b23d2afd4c6f3e0c7
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-10-03 12:47:49 -0500

    Setting up cookbook to mount iso

--- site-cookbooks/matrix/attributes/default.rb
+++ site-cookbooks/matrix/attributes/default.rb
@@ -1,2 +1,5 @@
 default['matrix']['iso_url'] = nil
 default['matrix']['compute_nodes'] = %w()
+default['matrix']['packages'] = %w(glibc.i686)
+default['matrix']['user'] = 'paraccel'
+default['matrix']['group'] = 'paraccel'
--- site-cookbooks/matrix/recipes/leader.rb
+++ site-cookbooks/matrix/recipes/leader.rb
@@ -1,12 +1,19 @@
 installer_iso = File.join(Chef::Config[:file_cache_path], 'padb.iso')
+installer_mount = '/mnt'
+
+user node['matrix']['user']
+group node['matrix']['group']
+
+node['matrix']['packages'].each do |pkg|
+  package pkg
+end
 
 remote_file installer_iso do
   source node['matrix']['iso_url']
   not_if { ::File.exist?(installer_iso) }
 end
 
-mount installer_iso do
-  mount_point '/mnt'
-  options 'loop'
-  action :mount
+execute "mount installer" do
+  command "mount -o loop #{installer_iso} #{installer_mount}"
+  not_if { ::File.exist?(File.join(installer_mount, 'install_padb')) }
 end
