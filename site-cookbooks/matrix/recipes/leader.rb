a78b012bffa5d74d415c6a513f2f90ad627fe9f1
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-10-06 16:39:08 -0500

    Update leader recipe to include some of the setup steps

--- site-cookbooks/matrix/recipes/leader.rb
+++ site-cookbooks/matrix/recipes/leader.rb
@@ -1,20 +1,49 @@
 installer_iso = File.join(Chef::Config[:file_cache_path], 'padb.iso')
-installer_mount = '/mnt'
+installer_mount = node['matrix']['installer_mount']
 
-yum_package 'glibc' do
-  arch 'i686'
+unless File.exist?('/home/paraccel/install_padb')
+  directory installer_mount do
+    owner 'root'
+    group 'root'
+    mode '0700'
+    action :create
+    not_if { File.exist?(installer_mount) }
+  end
+
+  yum_package 'glibc' do
+    arch 'i686'
+  end
+
+  node['matrix']['packages'].each do |pkg|
+    package pkg
+  end
+
+  remote_file installer_iso do
+    source node['matrix']['iso_url']
+    not_if { ::File.exist?(installer_iso) }
+  end
+
+  execute 'mount installer' do
+    command "mount -o loop #{installer_iso} #{installer_mount}"
+    not_if { ::File.exist?(File.join(installer_mount, 'install_padb')) }
+  end
 end
 
-node['matrix']['packages'].each do |pkg|
-  package pkg
-end
+# Phase 1 Install
+# 1 - Complete setup
+# 2 - Unmount iso, delete
 
-remote_file installer_iso do
-  source node['matrix']['iso_url']
-  not_if { ::File.exist?(installer_iso) }
-end
+# Patch files
+include_recipe 'matrix::patch'
 
-execute 'mount installer' do
-  command "mount -o loop #{installer_iso} #{installer_mount}"
-  not_if { ::File.exist?(File.join(installer_mount, 'install_padb')) }
-end
+# Phase 2 Install
+# 1 - Apply kernel params (sysctl -p /etc/sysctl.conf)
+execute 'sysctl -p /etc/sysctl.conf'
+
+# 2 - Create RAMdisk (create directory, mount -a, chown)
+directory node['matrix']['ramdisk']
+
+execute 'mount -a ; chown -R paraccel:paraccel /mnt/ramdisk'
+
+# 3 - start daemontools
+# 4 - Run phase 2 setup as paraccel user
